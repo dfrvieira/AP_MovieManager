533305e938bff0c06dccf9b9c8d27a7e4d25f910
dfrvieira <dfrvieira@users.noreply.github.com>
2019-12-02 14:50:45 +0000

    Update movie_manager.py

--- movie_manager.py
+++ movie_manager.py
@@ -88,3 +88,9 @@ def get_movies_by_genre(mm, genre):
         if movie['genre'] == genre:
             result.append(movie)
     return result
+
+def has_movie_with_actor_name(mm, actor_name):
+    pass
+
+def get_movie_by_actor_name(mm, actor_name):
+    pass
